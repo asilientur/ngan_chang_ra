9eba91aeb3a2b016f28681772065ca758567486f
asilientur <150160115+asilientur@users.noreply.github.com>
2023-11-07 14:51:11 +0000

    up

---
+++ b-i-l-i-c-o-m-i-c-s.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         b-i-l-i-c-o-m-i-c-s
+// @namespace    https://github.com/asilientur/ngan_chang_ra/raw/main/b-i-l-i-c-o-m-i-c-s.user.js
+// @version      2023.11.07
+// @description  down b-i-l-i-c-o-m-i-c-s
+// @author       You
+// @match        https://www.bilibilicomics.com/mc*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.bilibilicomics.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/asilientur/ngan_chang_ra/raw/main/b-i-l-i-c-o-m-i-c-s.user.js
+// @updateURL    https://github.com/asilientur/ngan_chang_ra/raw/main/b-i-l-i-c-o-m-i-c-s.meta.js
+// ==/UserScript==
